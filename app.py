0233282f44a8d10d1196f2d3bc50a6422509109c
AleOla30 <143103656+AleOla30@users.noreply.github.com>
2024-04-28 18:03:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from PIL import Image
 
 
 st.title('Análisis de datos de Sensores en Mi Ciudad')
-image = Image.open('grafana2.jpg')
-st.image(image)
 
 uploaded_file = st.file_uploader('Choose a file')
 
